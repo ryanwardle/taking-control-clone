94d33127cbb2f4485a36964c3190cebb88bc2b6f
ryanwardle <wardle2106@gmail.com>
2019-06-20 15:42:04 -0400

    update

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 onHoverOverTab = () => {
-  const panel = document.getElementById('news-panel');
+  // const classList = event.target.className.split(' ');
+  // console.log(event.target)
+  // const panelClass = event.target.innerText.replace(/ /g, "-").toLowerCase() + '-panel' ;
+  //
+  // console.log(classList);
+  // console.log(panelClass)
+  // const panel = document.getElementById(panelClass);
+  //
+  // if(classList.includes(panelClass)){
+  //   console.log(panelClass)
+  //   panel.style.zIndex = '10';
+
+
+  const panel = document.getElementById('news-and-insight-panel');
   panel.style.zIndex = '10';
 }
 
 onClosePanel = () => {
-  const panel = document.getElementById('news-panel');
+  const panel = document.getElementById('news-and-insight-panel');
   panel.style.zIndex = '-1';
 }
