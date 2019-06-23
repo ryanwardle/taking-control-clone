e2290f973087318096923311845e98d9a95cc97f
ryanwardle <wardle2106@gmail.com>
2019-06-23 19:14:38 -0400

    update

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-onHoverOverTab = () => {
-  // const classList = event.target.className.split(' ');
-  // console.log(event.target)
-  // const panelClass = event.target.innerText.replace(/ /g, "-").toLowerCase() + '-panel' ;
-  //
-  // console.log(classList);
-  // console.log(panelClass)
-  // const panel = document.getElementById(panelClass);
-  //
-  // if(classList.includes(panelClass)){
-  //   console.log(panelClass)
-  //   panel.style.zIndex = '10';
+getPanelId = () =>{
+  const classList = event.target.className.split(' ');
+  const panelId = event.target.innerText.replace(/ /g, "-").toLowerCase();
+  console.log(classList)
+  console.log(panelId)
+  if(classList.includes(panelId)){
+    console.log('yes')
+    onHoverOverTab(panelId +'-panel');
+  }
+}
 
-
-  const panel = document.getElementById('news-and-insight-panel');
+onHoverOverTab = (panelId) => {
+  const panel = document.getElementById(panelId);
   panel.style.zIndex = '10';
+  panel.style.visibility = 'visible'
 }
 
 onClosePanel = () => {
-  const panel = document.getElementById('news-and-insight-panel');
+  // const classList = event.target.className.split(' ');
+  // const panelId = event.target.innerText.replace(/ /g, "-").toLowerCase();
+  // const panel = document.getElementById(panelId);
+  // panel.style.zIndex = '-1';
+  // console.log(event)
+
+  const panel = document.getElementById();
   panel.style.zIndex = '-1';
+  panel.style.visibility = 'hidden';
+
+  console.log(event)
+
 }
+
+// onKeepTabOpen = () =>{
+//   const panel = document.getElementById(panelId);
+//   panel.style.zIndex = '10';
+// }
